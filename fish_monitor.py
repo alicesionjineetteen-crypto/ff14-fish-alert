43cf26a14584a1a47fc3541a99f4ba33ea93d841
alicesionjineetteen-crypto <alice.sionji.neetteen@gmail.com>
2026-08-21 13:48:56 +0900

    Delete fish_monitor.py

--- fish_monitor.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-ITEM_ID = 33242  # Ealad Skaan / イラッド・スカーン
-url = f"https://www.garlandtools.org/db/doc/fishing/ja/2/{ITEM_ID}.json"
-resp = requests.get(url)
-print(resp.status_code)
-print(resp.json())
